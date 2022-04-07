edeb64f52976be0140d88d0b08b43711894f10eb
mcagridemir <cagriidemirr@gmail.com>
2022-04-08 00:56:57 +0300

    Implement all features such as tabbar and view controllers, also detail page and navigation bar text field, and implement unit test

---
+++ iTunesApp/Scene/Four/Controller/CollectionView/ProdCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProdCollectionViewCell.swift
+//  iTunesApp
+//
+//  Created by Çağrı Demir on 6.04.2022.
+//
+
+import UIKit
+
+class ProdCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ iTunesApp/Scene/Third/Controller/CollectionView/ProductGridCollectionViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  ProductCollectionViewCell.swift
+//  iTunesApp
+//
+//  Created by Çağrı Demir on 5.04.2022.
+//
+
+import UIKit
+
+class ProductGridCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var artistLabel: UILabel!
+    @IBOutlet weak var trackLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    func initCell(product: Product?) {
+        artistLabel.text = product?.artistName
+        trackLabel.text = product?.trackName
+    }
+}
